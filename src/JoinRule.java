a42b85bd5b6b0e33ce63cf82c9745218a9009dc3
Navid Vafaei <navid.cs@gmaill.com>
2015-05-13 17:43:09 -0400

    JoinRule  is written

---
+++ src/JoinRule.java
@@ -0,0 +1,11 @@
+
+public class JoinRule {
+
+	  public String operator;
+	  public String table;
+	  public String fiel; 
+	   
+	  public JoinRule(String xmlDefinition){
+	    //parse rule
+	  }
+}
