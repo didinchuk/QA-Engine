bc590cb6d40a77a298261ba6d804bff251650210
Navid Vafaei <navid.cs@gmaill.com>
2015-05-13 17:40:55 -0400

    wrote  AggregateRule fields

---
+++ src/AggregateRule.java
@@ -0,0 +1,12 @@
+public class AggregateRule {
+
+	public String operator;
+	public String value;
+	public String aggregate;
+	public String target;
+
+	public AggregateRule(String xmlDefinition) {
+		// parse rule
+	}
+
+}
